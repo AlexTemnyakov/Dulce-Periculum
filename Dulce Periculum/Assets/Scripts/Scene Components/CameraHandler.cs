b53639990eca57c139ff91624ba7cf515efc46e6
alextemnyakov <alex.temnyakoff@yandex.ru>
2020-02-02 16:19:26 +0100

    camera corrected

--- Dulce Periculum/Assets/Scripts/Scene Components/CameraHandler.cs
+++ Dulce Periculum/Assets/Scripts/Scene Components/CameraHandler.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
+    public  float      DIST_FROM_PLAYER;
+    public  float      ANGLE;
+    public  float      HEIGHT;
+    private const
+            float      PLAYER_HEIGHT_OFFSET = 4.0f;
+    private const
+            float      CHANGE_POS_SPEED     = 0.8f;
+
     private GameObject player;
-    public  float      distFromPlayer;
-    public  float      angle;
-    public  float      height;
 
     void Start()
     {
@@ -22,14 +27,45 @@ public class CameraHandler : MonoBehaviour
 
     private void ChangePosition()
     {
-        Vector3 dir, newPos;
+        Vector3    dir, 
+                   newPos,
+                   tmp;
+        float      _dist    = DIST_FROM_PLAYER,
+                   _height  = HEIGHT;
 
-        dir                = player.transform.forward.normalized;
-        newPos             = player.transform.position + (-dir * distFromPlayer);
-        newPos.y           = player.transform.position.y + height;
-        transform.position = newPos;
-        transform.forward  = dir;
+        dir                 = player.transform.forward.normalized;
+        newPos              = player.transform.position + (-dir * _dist);
+        newPos.y            = player.transform.position.y + _height;
+        transform.position  = Vector3.Slerp(CorrectPosition(newPos), transform.position, CHANGE_POS_SPEED);
 
-        transform.Rotate(angle, 0, 0);
+        // Set the desired angle.
+        transform.forward          = player.transform.position - transform.position;
+        tmp                        = transform.localEulerAngles;
+        tmp.x                      = ANGLE;
+        transform.localEulerAngles = tmp;
+    }
+
+    private Vector3 CorrectPosition(Vector3 position)
+    {
+        Vector3[]  pointsToCheck = {
+                                            Vector3.zero,
+                                        new Vector3(15, -14, 0),
+                                        new Vector3(15, 14, 0)
+                                    };
+        Vector3    newPos = position;
+        Vector3    dir;
+        RaycastHit hit;
+
+        foreach (Vector3 v in pointsToCheck)
+        {
+            dir = Quaternion.Euler(v) * (position - (player.transform.position + Vector3.up * PLAYER_HEIGHT_OFFSET));
+            if (Physics.Raycast(player.transform.position + Vector3.up * PLAYER_HEIGHT_OFFSET, dir, out hit, DIST_FROM_PLAYER))
+            {
+                newPos = hit.point - dir.normalized * 2;
+                break;
+            }
+        }
+
+        return newPos;
     }
 }
